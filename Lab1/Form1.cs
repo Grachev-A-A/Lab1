5bbd1aa92f9e8dfbd638436d85e931cf409a8e77
Demon Tempter <sagrachov@gmail.com>
2018-10-10 13:42:14 +0300

    Working, no comments

---
+++ Lab1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1 {
+    public partial class Form1 : Form {
+
+        enum Direction { Right, Down, Left, Up };
+        Direction direction = Direction.Right;
+
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) { 
+           
+            switch (direction) {
+                case Direction.Right:
+                    label1.Top = 0;
+                    label1.Left += 10;
+                    if (label1.Right > ClientRectangle.Width) {
+                        direction = Direction.Down;
+                        label1.Left = ClientRectangle.Width - label1.Width;
+                    }
+                    label1.ForeColor = Color.FromArgb(255, 255 - label1.Left * 255 / (ClientRectangle.Width - label1.Width), 0);
+                    break;
+                case Direction.Down:
+                    label1.Left = ClientRectangle.Width - label1.Width;
+                    label1.Top += 10;
+                    if (label1.Top > ClientRectangle.Height - label1.Height) {
+                        direction = Direction.Left;
+                        label1.Top = ClientRectangle.Height - label1.Height;
+                    }
+                    label1.ForeColor = Color.FromArgb(255 - label1.Top * 255 / (ClientRectangle.Height + label1.Height), label1.Top * 255 / (ClientRectangle.Height + label1.Height), label1.Top * 255 / (ClientRectangle.Height + label1.Height));
+                    break;
+                case Direction.Left:
+                    label1.Left -= 10;
+                    label1.Top = ClientRectangle.Height - label1.Height;
+                    if (label1.Left < 0) {
+                        direction = Direction.Up;
+                        label1.Left = 0;
+                    }
+                    label1.ForeColor = Color.FromArgb(0, 255, label1.Left * 255 / (ClientRectangle.Width + label1.Width));
+                    break;
+                case Direction.Up:
+                    label1.Top -= 10;
+                    label1.Left = 0;
+                    if (label1.Bottom < label1.Height) {
+                        direction = Direction.Right;
+                        label1.Top = 0;
+                    }
+                    label1.ForeColor = Color.FromArgb(255 - label1.Top * 255 / (ClientRectangle.Height - label1.Height), 255, 0);
+                    break;
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e) {
+            Form2 form2 = new Form2();
+            form2.form1 = this;
+            form2.Show();
+            form2.Left = ClientRectangle.Width + 15; // Сдвиг, чтобы формы не перекрывали друг друга
+           
+        }
+    }
+}
---
+++ Lab1/Form2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1 {
+    public partial class Form2 : Form {
+
+        public Form1 form1;
+        public Form2() {
+            InitializeComponent();
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            form1.timer1.Enabled = comboBox1.SelectedIndex == 0;
+        }
+
+        private void Form2_Shown(object sender, EventArgs e) {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if(textBox1.Lines.Length == 0) {
+                MessageBox.Show("В поле нет ни одной строки!");
+                return;
+            }
+
+            string[] tmp = new string[textBox1.Lines.Length - 1];
+            for (int i = 0; i < tmp.Length; i++) {
+                tmp[i] = textBox1.Lines[i + 1];
+            }
+            textBox1.Lines = tmp;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            Random rnd = new Random();
+            string src = "AaBbCcDdEeFfGgHhIiGgKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщъЫыьЭэЮюЯя";
+            string str = "";
+            for (int i = 0; i < rnd.Next(1, 33); i++) {
+                str += src[rnd.Next(src.Length)];
+            }
+            textBox1.AppendText(str + "\n");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+            label1.Text = $"Количество строк: {(textBox1.Lines.Length == 0 ? 0 : textBox1.Lines.Length -1)}";
+        }
+    }
+}
